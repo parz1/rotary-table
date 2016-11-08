fdc085dc80d668b3118c3bc11b2ada47ac0c746c
eskab <szymon.kabelis@yahoo.com>
2016-11-08 14:27:05 +0100

    Concatenate all scripts into one file

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./delete-repo.sh
-./install-deps.sh
-./delete-scripts.sh
-./create-new-repo.sh
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+name="CreateJS Boilerplate"
+separator="-----------------------------------------------------------"
+
+# deleting git repository
+echo "$separator"
+echo "[$name] Deleting repository"
+echo "$separator"
+rm -rf ".git"
+echo "[$name] Complete"
+
+# installing npm and bower dependencies
+echo "$separator"
+echo "[$name] Installing dependencies"
+echo "$separator"
+npm install
+echo "[$name] NPM dependencies installed"
+bower install
+echo "[$name] Bower dependencies installed"
+echo "[$name] Complete"
+
+# creating new repository
+echo "$separator"
+echo "[$name] Initializing new repository"
+echo "$separator"
+git init && git add . && git commit -m "Initial commit"
+echo "[$name] Complete"
+
+# self deleting
+rm -rf "setup.sh"

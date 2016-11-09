51f3b2866dfdd145e96263249646cbf535591f12
eskab <szymon.kabelis@yahoo.com>
2016-11-09 12:03:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,47 @@
 
 name="CreateJS Boilerplate"
 separator="-----------------------------------------------------------"
+timestamp() {
+  date +"%T"
+}
+clearProcess() {
+  PID=$!
+  disown $PID
+  kill -9 $PID
+  printf "\n"
+}
+executeCommand() {
+  printf "Please wait"
+  while sleep 0.5; do
+    printf "."
+  done & $1
+  clearProcess
+}
+currentTime=`timestamp`
 
 # deleting git repository
 echo "$separator"
-echo "[$name] Deleting repository"
+echo "[$currentTime][$name] Deleting repository"
 echo "$separator"
-rm -rf ".git"
-echo "[$name] Complete"
+rm -rf .git
+echo "[$currentTime][$name] Complete"
 
 # installing npm and bower dependencies
 echo "$separator"
-echo "[$name] Installing dependencies"
+echo "[$currentTime][$name] Installing dependencies"
 echo "$separator"
-npm install
-echo "[$name] NPM dependencies installed"
-bower install
-echo "[$name] Bower dependencies installed"
-echo "[$name] Complete"
+executeCommand $"npm install"
+echo "[$currentTime][$name] NPM dependencies installed"
+executeCommand $"bower install"
+echo "[$currentTime][$name] Bower dependencies installed"
+echo "[$currentTime][$name] Complete"
 
 # creating new repository
 echo "$separator"
-echo "[$name] Initializing new repository"
+echo "[$currentTime][$name] Initializing new repository"
 echo "$separator"
-git init && git add -u && git commit -m "Initial commit"
-echo "[$name] Complete"
+git init && git add . && git commit -m "Initial commit"
+echo "[$currentTime][$name] Complete"
 
 # self deleting
 rm -rf "setup.sh"

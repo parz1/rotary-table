71a38c54795d756595d220d4c274475c7938ad0a
eskab <szymon@kabelis.pl>
2017-11-10 20:44:35 +0100

    Refactored setup script & updated yarn.lock

--- setup.sh
+++ setup.sh
@@ -5,19 +5,6 @@ separator="-----------------------------------------------------------"
 timestamp() {
   date +"%T"
 }
-clearProcess() {
-  PID=$!
-  disown $PID
-  kill -9 $PID
-  printf "\n"
-}
-executeCommand() {
-  printf "Please wait"
-  while sleep 0.5; do
-    printf "."
-  done & $1
-  clearProcess
-}
 currentTime=`timestamp`
 
 # deleting git repository
@@ -25,16 +12,6 @@ echo "$separator"
 echo "[$currentTime][$name] Deleting repository"
 echo "$separator"
 rm -rf .git
-echo "[$currentTime][$name] Complete"
-
-# installing npm and bower dependencies
-echo "$separator"
-echo "[$currentTime][$name] Installing dependencies"
-echo "$separator"
-executeCommand $"npm install"
-echo "[$currentTime][$name] NPM dependencies installed"
-executeCommand $"bower install"
-echo "[$currentTime][$name] Bower dependencies installed"
 echo "[$currentTime][$name] Complete"
 
 # creating new repository

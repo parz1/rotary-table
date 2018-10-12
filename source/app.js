9e4e8cf8ba656d382843fc9ec4bde94e5a999069
Szymon Kabelis <szymon@kabelis.pl>
2018-10-12 21:22:32 +0200

    - add newest webpack, webpack config - add app entry point and styles - add prettier

---
+++ source/app.js
@@ -0,0 +1,17 @@
+import { createjs } from "createjs";
+import { createDOMStage, getDOMStage } from "@/utils";
+
+import "./styles.css";
+
+const init = () => {
+  const body = document.getElementsByTagName("body")[0];
+  const canvas = createDOMStage();
+  body.append(canvas);
+
+  window.onload = () => {
+    const canvas = getDOMStage();
+    const stage = new createjs.Stage(canvas);
+  };
+};
+
+init();
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const rootPath = path.resolve(__dirname, "source");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
+
+module.exports = {
+  context: rootPath,
+  entry: {
+    createjs: path.join(nodeModulesPath, "/createjs/builds/1.0.0/createjs.js"),
+    app: path.join(rootPath, "/app.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devServer: {
+    contentBase: rootPath,
+    stats: {
+      warnings: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /node_modules(\/|\\)(createjs)(\/|\\).*\.js$/,
+        loader: "imports-loader?this=>window!exports-loader?window.createjs",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "CreateJS Boilerplate",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": rootPath,
+      createjs: path.resolve(__dirname, "source/createjs"),
+    },
+  },
+};

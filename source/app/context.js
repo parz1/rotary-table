63ae804ae5e3a30a96e60f963cb243373398c167
Szymon Kabelis <szymon@kabelis.pl>
2018-10-12 19:49:48 +0200

    E R A S E

--- source/app/context.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prefer-arrow-callback */
-import App from '@/App';
-
-window.addEventListener('load', () => {
-  App.service('context', function (config) {
-    return document.getElementById(config.canvas.id);
-  }, 'config');
-});
